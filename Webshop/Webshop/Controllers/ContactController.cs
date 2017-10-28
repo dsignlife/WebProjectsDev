5afdef2af2e89c0db3ff05ac45ba284381f9e837
Tomz Phoz <dsign-life@hotmail.com>
2017-10-28 18:12:30 +0200

    Add ContactEmailServices linked to gmail

--- Webshop/Webshop/Controllers/ContactController.cs
+++ Webshop/Webshop/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Webshop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,5 +14,43 @@ namespace Webshop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index([Bind("Subject,Name,Email,Message")] ContactViewModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+
+                    msg.From = new MailAddress(contact.Email);
+                    msg.To.Add("contact.webproject@gmail.com");
+                    msg.Subject = contact.Subject;
+                    msg.Body = contact.Message + "\nEmail: " + contact.Email + "\nName: " + contact.Name;
+
+                    SmtpClient smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        Credentials = new System.Net.NetworkCredential("contact.webproject@gmail.com", "Admin=1337"),
+                        EnableSsl = true
+                    };
+
+                    smtp.Send(msg);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for contacting us!";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $"Sorry, a problem occurred {ex.Message}";
+                }
+            }
+
+            return View();
+        }
     }
+    
 }
---
+++ Webshop/Webshop/ViewModels/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webshop.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Subject required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "ERROR")]
+        [StringLength(4096, MinimumLength = 10)]
+        public string Message { get; set; }
+    }
+}
